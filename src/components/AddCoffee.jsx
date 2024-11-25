f6ecc9cd51ea673d58996f944978b69d4d97f628
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-26 00:14:30 +0600

    Add Coffee started

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,11 +1,33 @@
-
-
 const AddCoffee = () => {
-    return (
-        <div>
-            <h2>Add Coffee</h2>
+  return (
+    <div>
+      <h2>Add Coffee</h2>
+      <form>
+        <div className="flex">
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Coffee Name</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Coffee Name"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Available Quantity</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Available Quantity"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
