ae5ed2ce2212a93a42785e57932a1a38ddca7232
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-26 13:23:34 +0600

    Create operation done and sweet alert implemented

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,18 +1,48 @@
+import Swal from "sweetalert2";
+
 const AddCoffee = () => {
+  const handleAddCoffee = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
 
-    const handleAddCoffee= (event) => {
-        event.preventDefault();
-        const form = event.target;
-        const name = form.name.value;
-        const quantity = form.quantity.value;
-        const supplier = form.supplier.value;
-        const taste = form.taste.value;
-        const category = form.category.value;
-        const details = form.details.value;
-        const photo = form.photo.value;
+    const newCoffee = {
+      name,
+      quantity,
+      supplier,
+      taste,
+      category,
+      details,
+      photo,
+    };
 
-        const newCoffee = {name, quantity, supplier, taste, category, details, photo}
-    }
+    // Send Data to The Server
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "Coffee Added Successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
+  };
 
   return (
     <div className="bg-[#F4F3F0] p-24 ">
@@ -108,8 +138,11 @@ const AddCoffee = () => {
           </label>
         </div>
 
-        <input type="submit" value="Add Coffee" className="btn btn-block btn-neutral" />
-
+        <input
+          type="submit"
+          value="Add Coffee"
+          className="btn btn-block btn-neutral"
+        />
       </form>
     </div>
   );
